b030f3257094795d67fc540c3e0d31702d50850e
Anatoliy Popov <popov.anatoly@gmail.com>
2019-02-04 17:07:38 +0300

    switched to django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+def docs(request):
+    return render(request, 'mainapp/doc.html')
